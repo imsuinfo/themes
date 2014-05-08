89f13d23b8565ebc297080e49817dc7454659b06
Kevin Day <kday@mcneese.edu>
2014-05-08 15:23:13 -0500

    Update: add the custom 'Event Requests' link to the toolbar

--- template.php
+++ template.php
@@ -25,8 +25,36 @@ function mcneese_event_workflow_preprocess_page(&$vars) {
  * Render all data for: page.
  */
 function mcneese_event_workflow_render_page() {
-  global $base_url;
   $cf = & drupal_static('cf_theme_get_variables', array());
+
+}
+
+/**
+ * Implements hook_preprocess_toolbar().
+ */
+function mcneese_event_workflow_preprocess_toolbar(&$vars) {
+  $cf = & drupal_static('cf_theme_get_variables', array());
+
+  $events_tree = menu_build_tree('navigation', array(
+    'conditions' => array('ml.link_path' => 'events'),
+    'min_depth' => 1,
+    'max_depth' => 1,
+  ));
+
+  if (!function_exists('toolbar_menu_navigation_links')) {
+    return;
+  }
+
+  if (!function_exists('mcneese_event_workflow_menu')) {
+    return;
+  }
+
+  $toolbar_tree = toolbar_get_menu_tree();
+  $tree = array_merge($events_tree, $toolbar_tree);
+
+  $links = toolbar_menu_navigation_links($tree);
+  $vars['toolbar']['toolbar_menu']['#links'] = $links;
+
 }
 
 /**
