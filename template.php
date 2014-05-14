534a174787fa7fab3fdbb3a59a55525e24422eac
Kevin Day <kday@mcneese.edu>
2014-05-14 16:56:25 -0500

    Cache: save current work on theme design
    Note: there is a logic flaw in the even/odd generation of the row.
    I want to save the current setup now before I try to weed out that mistake.

--- template.php
+++ template.php
@@ -17,7 +17,11 @@
  * Implements hook_preprocess_page().
  */
 function mcneese_event_workflow_preprocess_page(&$vars) {
-  $cf = & drupal_static('cf_theme_get_variables', array());
+  $cf = &drupal_static('cf_theme_get_variables', array());
+
+  if (empty($cf)) {
+    mcneese_initialize_variables($vars);
+  }
 }
 
 
@@ -25,15 +29,45 @@ function mcneese_event_workflow_preprocess_page(&$vars) {
  * Render all data for: page.
  */
 function mcneese_event_workflow_render_page() {
-  $cf = & drupal_static('cf_theme_get_variables', array());
+  $cf = &drupal_static('cf_theme_get_variables', array());
 
+  // build & render precrumb
+  $cf['data']['page']['precrumb'] = '';
+  $cf['show']['page']['precrumb'] = FALSE;
+
+  if (isset($cf['user']['object']) && is_object($cf['user']['object']) && $cf['user']['object']->uid > 0) {
+    $path_parts = explode('/', $cf['at']['path']);
+    $count_parts = count($path_parts);
+
+    if ($count_parts > 1 && $path_parts[0] == 'events') {
+      if ($count_parts > 2) {
+        if ($path_parts[1] == 'view-0' && cf_is_integer($path_parts[2])) {
+          $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "Request " . $path_parts[2] . '</div>';
+          $cf['show']['page']['precrumb'] = TRUE;
+        }
+        elseif ($path_parts[1] == 'edit-0' && cf_is_integer($path_parts[2])) {
+          $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "Request " . $path_parts[2] . '</div>';
+          $cf['show']['page']['precrumb'] = TRUE;
+        }
+      }
+
+      if ($path_parts[1] == 'create-0') {
+        $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "New Request" . '</div>';
+        $cf['show']['page']['precrumb'] = TRUE;
+      }
+    }
+  }
 }
 
 /**
  * Implements hook_preprocess_toolbar().
  */
 function mcneese_event_workflow_preprocess_toolbar(&$vars) {
-  $cf = & drupal_static('cf_theme_get_variables', array());
+  $cf = &drupal_static('cf_theme_get_variables', array());
+
+  if (empty($cf)) {
+    mcneese_initialize_variables($vars);
+  }
 
   $events_tree = menu_build_tree('navigation', array(
     'conditions' => array('ml.link_path' => 'events'),
---
+++ templates/page.tpl.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @file
+ * Page theme implementation.
+ */
+  $cf = & drupal_static('cf_theme_get_variables', array());
+
+  mcneese_render_page();
+  mcneese_event_workflow_render_page();
+
+  $float_side = in_array('fixed', $cf['page']['tags']['mcneese_page_side_open']['attributes']['class']);
+  $split_page = !$float_side && ($cf['show']['page']['menus'] || $cf['show']['page']['asides']);
+?>
+
+<?php mcneese_do_print($cf, 'page_header'); ?>
+
+<?php mcneese_do_print($cf, 'messages', FALSE); ?>
+<?php mcneese_do_print($cf, 'help', FALSE); ?>
+<?php mcneese_do_print($cf, 'information', FALSE); ?>
+
+<div id="mcneese-float-right" class="expanded fixed">
+  <?php mcneese_do_print($cf, 'messages', TRUE, TRUE); ?>
+  <?php mcneese_do_print($cf, 'help', TRUE, TRUE); ?>
+  <?php mcneese_do_print($cf, 'information', TRUE, TRUE); ?>
+  <?php mcneese_do_print($cf, 'work_area_menu', TRUE, TRUE); ?>
+</div>
+
+<?php if ($split_page) { ?>
+  <div id="mcneese-page-content" class="mcneese-content split" role="main">
+    <?php mcneese_do_print($cf, 'side', FALSE); ?>
+
+    <div class="column-2">
+<?php } else { ?>
+  <div id="mcneese-page-content" class="mcneese-content full" role="main">
+<?php } ?>
+
+  <?php mcneese_do_print($cf, 'page_title'); ?>
+
+  <div id="mcneese-float-left" class="expanded fixed">
+    <?php mcneese_do_print($cf, 'menu_tabs'); ?>
+    <?php mcneese_do_print($cf, 'action_links'); ?>
+    <?php mcneese_do_print($cf, 'breadcrumb'); ?>
+    <?php mcneese_do_print($cf, 'side'); ?>
+  </div>
+
+  <?php mcneese_do_print($cf, 'menu_tabs', FALSE); ?>
+  <?php mcneese_do_print($cf, 'breadcrumb', FALSE); ?>
+  <?php mcneese_do_print($cf, 'action_links', FALSE); ?>
+  <?php mcneese_do_print($cf, 'watermarks-pre'); ?>
+
+  <div id="mcneese-content-main" class="mcneese-content-main" role="main">
+    <!--(begin-page-main)-->
+    <?php if ($cf['show']['page']['content']) { ?>
+      <?php print($cf['data']['page']['content']); ?>
+    <?php } ?>
+    <!--(end-page-main)-->
+  </div>
+
+  <?php mcneese_do_print($cf, 'watermarks-post'); ?>
+
+<?php if ($split_page) { ?>
+    </div>
+  </div>
+<?php } else { ?>
+  </div>
+<?php } ?>
+
+<?php mcneese_do_print($cf, 'page_footer'); ?>
