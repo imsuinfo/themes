1788b78e9e779608de4c088e916167177b71f2e8
Kevin Day <kday@mcneese.edu>
2014-08-12 15:42:31 -0500

    Bugfix: fix breadcrumb problems and add support to the bill breadcrumb

--- template.php
+++ template.php
@@ -47,7 +47,7 @@ function mcneese_event_workflow_render_page() {
     if ($count_parts > 0 && $path_parts[0] == 'events') {
       if ($count_parts == 1) {
         if ($path_parts[0] == 'events') {
-          $title = "Event Dashboard";
+          $title = "Event Requests Dashboard";
 
           $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . $title . '</div>';
           $cf['show']['page']['precrumb'] = TRUE;
@@ -141,17 +141,26 @@ function mcneese_event_workflow_render_page() {
             $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/' . $path_parts[1] . '/' . $path_parts[2] . '" title="Edit Request">' . "Edit Request" . '</a>';
             $rebuild_breadcrumb = TRUE;
           }
-          elseif ($path_parts[1] == 'history-0' && cf_is_integer($path_parts[2])) {$cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "Request " . $path_parts[2] . '</div>';
+          elseif ($path_parts[1] == 'history-0' && cf_is_integer($path_parts[2])) {
+            $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "Request " . $path_parts[2] . '</div>';
             $cf['show']['page']['precrumb'] = TRUE;
 
-            $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/view-0/' . $path_parts[2] . '" title="Request Request">' . "View Request" . '</a>';
+            $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/view-0/' . $path_parts[2] . '" title="View Request">' . "View Request" . '</a>';
             $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/' . $path_parts[1] . '/' . $path_parts[2] . '" title="Request History">' . "Request History" . '</a>';
+            $rebuild_breadcrumb = TRUE;
+          }
+          elseif ($path_parts[1] == 'bill-0' && cf_is_integer($path_parts[2])) {
+            $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . "Request " . $path_parts[2] . '</div>';
+            $cf['show']['page']['precrumb'] = TRUE;
+
+            $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/view-0/' . $path_parts[2] . '" title="View Request">' . "View Request" . '</a>';
+            $cf['page']['breadcrumb'][] = '<a href="' . $base_path . 'events/' . $path_parts[1] . '/' . $path_parts[2] . '" title="Estimated Bill of Charges">' . "Request Bill" . '</a>';
             $rebuild_breadcrumb = TRUE;
           }
         }
 
         if (!$rebuild_breadcrumb) {
-          $title = "Event Dashboard";
+          $title = "Event Requests Dashboard";
 
           $cf['data']['page']['precrumb'] = '<div class="crumb-event_id">' . $title . '</div>';
           $cf['show']['page']['precrumb'] = TRUE;
